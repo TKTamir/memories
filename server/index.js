7c5a03102b6294bd5e025a2319d71d318dd79e8f
TKTamir <104828119+TKTamir@users.noreply.github.com>
2022-12-15 16:25:52 +0100

    Set up api routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import postRoutes from './routes/posts.js';
+
 const app = express();
+//localhost:5000/posts
+app.use('/posts', postRoutes);
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
@@ -12,3 +15,9 @@ app.use(cors());
 
 const CONNECTION_URL =
   'mongodb+srv://themovieguy:thepasswordguy@appformovies.ee8zf.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+  .catch((error) => console.log(error.message));
